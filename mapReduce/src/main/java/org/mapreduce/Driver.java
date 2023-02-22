890ad01e40fea123cb674ff2851c6d2dbc8a5649
ManduTheCat <krocd@naver.com>
2023-02-22 17:52:45 +0900

    project init

--- mapReduce/src/main/java/org/mapreduce/Driver.java
+++ mapReduce/src/main/java/org/mapreduce/Driver.java
@@ -1,22 +1,38 @@
 package org.mapreduce;
 
-import org.apache.hadoop.util.ProgramDriver;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class Driver {
-	public static void main(String[] args) {
-		int exitCode = -1;
-		ProgramDriver pgd = new ProgramDriver();
-		try {
-
-			pgd.addClass("wordcount", Wordcount.class, "A map/reduce program that performs word counting.");
-
-      			pgd.driver(args);
-			exitCode = 0;
-		}
-		catch(Throwable e) {
-			e.printStackTrace();
+	public static void main(String[] args) throws Exception{
+		Configuration conf = new Configuration();
+		if (args.length != 2){
+			System.err.println("Usage: WordCount <input> <output>");
+			System.exit(2);
 		}
 
-		System.exit(exitCode);
+		Job job = new Job(conf, "WordClass");
+
+		job.setJarByClass(Driver.class);
+		job.setMapperClass(WordCountMapper.class);
+		job.setReducerClass(WordCountReducer.class);
+
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		job.waitForCompletion(true);
 	}
 }
