8eb401a2ff2bd5364e24d6eb86ab865c19a859e8
ManduTheCat <krocd@naver.com>
2023-02-23 17:49:51 +0900

    add WordCount1char fix 한글 포맷팅

--- src/main/java/org/example/Wordcount1char.java
+++ src/main/java/org/example/Wordcount1char.java
@@ -1,67 +1,50 @@
 package org.example;
 
-import java.io.IOException;
-import java.util.StringTokenizer;
-
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
-public class WordCount1char {
+public class Wordcount1char {
 	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
 		private final static IntWritable one = new IntWritable(1);
 		private Text word = new Text();
 
-		public void map(Object key, Text val, Context context) throws IOException, InterruptedException {
-			StringTokenizer st = new StringTokenizer(val.toString());
-			while (!st.hasMoreTokens()) {
-				word.set(st.nextToken().substring(0, 1));
-				//한글자만 가져와서 키값으로 보넨다
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while(itr.hasMoreTokens()) {
+				word.set(itr.nextToken().substring(0, 1));
 				context.write(word, one);
 			}
 		}
-
 	}
-	public static class IntSumReducer
-		extends Reducer<Text,IntWritable,Text,IntWritable> {
-		private IntWritable result = new IntWritable();
 
-		public void reduce(Text key, Iterable<IntWritable> values,
-			Context context
-		) throws IOException, InterruptedException {
-			int sum = 0;
-			for (IntWritable val : values) {
-				sum += val.get();
-			}
-			result.set(sum);
-			context.write(key, result);
-		}
-	}
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
 		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+
 		if(otherArgs.length != 2) {
 			System.err.println("Usage: <in> <out>");
 			System.exit(2);
 		}
-		Job job = new Job(conf, "word_count");
-		job.setJarByClass(WordCount1char.class);
-		job.setMapOutputKeyClass(TokenizerMapper.class);
 
-		job.setReducerClass(IntSumReducer.class);
+		Job job = new Job(conf, "word_count");
+		job.setJarByClass(Wordcount1char.class);
+		job.setMapperClass(TokenizerMapper.class);
+
+		job.setReducerClass(WordCount.IntSumReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 		job.setNumReduceTasks(2);
 		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
 		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
-
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
